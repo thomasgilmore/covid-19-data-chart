2b86d1cdd2442c3d7c99483ce64450bcc6e5889d
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-01 18:39:33 -0500

    created new cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -17,6 +17,22 @@ const Cards = (props) => {
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
             </Grid>
         </div>
     )
