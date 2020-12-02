5ad673f8ae4ad854d5049c1206afd18d93c051d8
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-02 11:33:24 -0500

    Added Date to Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -18,7 +18,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography variant="h5">
                             <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
                         </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
