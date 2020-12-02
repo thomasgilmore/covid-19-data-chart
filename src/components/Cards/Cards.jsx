0e61efd50b6fc937ae0163284e7ba4ef507cf2a9
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-02 11:27:04 -0500

    working on cards components with data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if(!confirmed) {
+        return 'Loading....';
+    }
 
     return (
         <div className={styles.container}>
@@ -12,7 +15,9 @@ const Cards = (props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                        </Typography>
                         <Typography color="textSecondary">REAL DATE</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
